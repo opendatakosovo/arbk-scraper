47c0318d3acdf7697c99c8e54086c2ee18ae2c67
Georges Labreche <georges.labreche@gmail.com>
2017-05-11 13:11:38 +0200

    list in a text file reg num of errors we should retry

--- check_errors.rb
+++ check_errors.rb
@@ -1,11 +1,14 @@
 require 'mongo'
+require 'FileUtils'
 
 Mongo::Logger.logger.level = ::Logger::FATAL
 
+# Delete file created from last run.
+# This file lists all of the reg nums we should retry.
+FileUtils.rm('retry.txt')
+
 # Establish connection to database
 client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'arbk')
-#$collection_businesses = client[:businesses]
-#$collection_errors = client[:errors]
 $collection_businesses = client[:businesses]
 $collection_errors = client[:errors]
 
@@ -14,14 +17,14 @@ error_types_to_skip = ['browser window was closed']
 
 all_error_reg_nums = $collection_errors.distinct('registrationNum')
 
-# Get different error types:
+# Get different error types.
 error_types = $collection_errors.aggregate([{
     '$group' => {
         '_id' => '$errorMsg',
         'count' => {'$sum' => 1} }
     }])
 
-
+# Display summary/stats of error types.
 for error_type in error_types
     if not error_types_to_skip.include?(error_type['_id'])
         puts
@@ -31,20 +34,37 @@ for error_type in error_types
         puts error_type['_id']
         puts
 
+        # These are all the errors recorded.
         error_reg_nums = $collection_errors.distinct('registrationNum', {
             'errorMsg' => error_type['_id'],
             'registrationNum' => {'$gt' => 0}})
 
-        scraped_reg_nums = $collection_businesses.distinct('formatted.registrationNum', {
+        # These are all the errors that have been recovered.
+        recovered_error_reg_nums = $collection_businesses.distinct('formatted.registrationNum', {
             'formatted.registrationNum' => {
                 '$in' => error_reg_nums
             }
         })
 
+        # There are all the errors that we are not sure have been recovered so we need to try again
+        unrecovered_error_reg_nums = $collection_errors.distinct('registrationNum', {
+                'errorMsg' => error_type['_id'],
+                'registrationNum' => {
+                    '$nin' => recovered_error_reg_nums
+                }
+            })
+
+        # Compilre all reg nums we should retry:
+        File.open('retry.txt', 'a') do |f|
+            f.puts(unrecovered_error_reg_nums)
+        end
+
         puts 'Number of errors triggered: ' + error_reg_nums.length.to_s
-        puts 'Number of errors recovered: ' + scraped_reg_nums.length.to_s
-        puts 'Number of errors requiring checking: ' + (error_reg_nums.length - scraped_reg_nums.length).to_s
+        puts 'Number of errors recovered: ' + recovered_error_reg_nums.length.to_s
+        puts 'Number of errors requiring checking: ' + unrecovered_error_reg_nums.length.to_s
         puts
         puts
     end
 end
+
+
